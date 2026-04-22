7af16cfe98d762e24edc1544dbd79186e9040309
Soufiane Asrih <soufasri@emeal.nttdata.com>
2026-04-22 16:11:38 +0100

    Deploy GitHub Pages

--- vite.config.ts
+++ vite.config.ts
@@ -1,14 +1,16 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import { TanStackRouterVite } from '@tanstack/router-plugin/vite'
+import tailwindcss from '@tailwindcss/vite'
 
 export default defineConfig({
   base: '/text-extractor-hub/',
   plugins: [
-    TanStackRouterVite(),
+    TanStackRouterVite({ autoCodeSplitting: true }),
     react(),
+    tailwindcss(),
   ],
   build: {
     outDir: 'dist',
-  }
+  },
 })
